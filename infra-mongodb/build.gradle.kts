42171398cdd103dae1373123d37118ded4e44b9a
wonsim02 <wonsim0718@gmail.com>
2024-02-03 22:46:45 +0900

    test setting

--- infra-mongodb/build.gradle.kts
+++ infra-mongodb/build.gradle.kts
@@ -1,4 +1,9 @@
 dependencies {
     api(springBoot("starter-data-mongodb"))
     implementation(project(":common"))
+
+    testImplementation(platform(testContainersBom()))
+    testImplementation(springBoot("starter-test"))
+    testImplementation(testContainers("junit-jupiter"))
+    testImplementation(testContainers("mongodb"))
 }
