44b9e975c0b23f643e6a3fde8294b90fef593fbc
wonsim02 <wonsim0718@gmail.com>
2024-03-03 22:22:31 +0900

    add test using classes generated by MongoDocumentPropertiesProcessor

--- infra-mongodb/src/test/kotlin/com/github/wonsim02/infra/mongodb/testcase/document/Document0.kt
+++ infra-mongodb/src/test/kotlin/com/github/wonsim02/infra/mongodb/testcase/document/Document0.kt
@@ -1,11 +1,23 @@
 package com.github.wonsim02.infra.mongodb.testcase.document
 
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.Instant
 
 @Document(collection = Document0.COLLECTION_NAME)
 data class Document0(
     val id: Long,
+    val a: List<A>,
+    val f: Float,
 ) {
+    data class A(
+        val b: List<B>,
+        val c: Int,
+    )
+
+    data class B(
+        val d: String,
+        val e: Instant,
+    )
 
     companion object {
 
