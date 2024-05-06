0a7d1b1271620e5d3ee9e82ddc56229d2372cedf
wonsim02 <wonsim0718@gmail.com>
2024-05-06 16:39:45 +0900

    register RedisTemplate<String, ByteArray> bean

---
+++ infra-redis/src/main/kotlin/com/github/wonsim02/infra/redis/config/CustomizingRedisPropertiesConfiguration.kt
@@ -0,0 +1,37 @@
+package com.github.wonsim02.infra.redis.config
+
+import com.github.womsim02.common.spring.ProfileAwarePropertySource
+import org.springframework.boot.autoconfigure.AutoConfigureBefore
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+
+/**
+ * [RedisAutoConfiguration] 이전에 [RedisProperties]를 수정하는 configuration.
+ */
+@EnableConfigurationProperties(
+    InfraRedisProperties::class,
+    RedisProperties::class,
+)
+@ProfileAwarePropertySource(
+    locations = [
+        "classpath:/infra-redis-config.yml",
+        "classpath:/infra-redis-config-*.yml",
+    ],
+)
+@AutoConfigureBefore(RedisAutoConfiguration::class)
+class CustomizingRedisPropertiesConfiguration(
+    infraRedisProperties: InfraRedisProperties,
+    redisProperties: RedisProperties,
+) {
+
+    init {
+        // `infraRedisProperties.clustered`가 `true`이면 `redisProperties.cluster` 값을 설정해 준다.
+        if (infraRedisProperties.clustered) {
+            val endpoint = redisProperties.run { "$host:$port" }
+            val cluster = RedisProperties.Cluster()
+            cluster.nodes = mutableListOf(endpoint)
+            redisProperties.cluster = cluster
+        }
+    }
+}
